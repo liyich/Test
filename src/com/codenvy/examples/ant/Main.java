0635ceda450ed7a1237fb71a139d5b273fa78df0
lee <coding16@gmail.com>
2016-07-24 10:54:56 +0000

    init

---
+++ src/com/codenvy/examples/ant/Main.java
@@ -0,0 +1,9 @@
+package com.codenvy.examples.ant;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hell World");
+        System.out.println("hhhhhhhh");
+      	System.out.println('hello');
+    }
+}
